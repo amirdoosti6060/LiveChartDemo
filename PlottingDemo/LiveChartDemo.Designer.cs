7b805bd52bf1e2a63f90ae35e33f8db71d27a67b
Amir Doosti <amirdoosti@hotmail.com>
2024-10-19 18:03:51 +0200

    Add project files.

---
+++ PlottingDemo/LiveChartDemo.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace PlottingDemo
+{
+    partial class LiveChartDemo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            chart = new LiveChartsCore.SkiaSharpView.WinForms.CartesianChart();
+            menuStrip1 = new MenuStrip();
+            mnuInteraction = new ToolStripMenuItem();
+            mnuZoomAll = new ToolStripMenuItem();
+            mnuZoomX = new ToolStripMenuItem();
+            mnuZoomY = new ToolStripMenuItem();
+            mnuZoomBoth = new ToolStripMenuItem();
+            mnuPan = new ToolStripMenuItem();
+            mnuZoomBothPan = new ToolStripMenuItem();
+            mnuZoomNone = new ToolStripMenuItem();
+            legendToolStripMenuItem = new ToolStripMenuItem();
+            mnuLegenHidden = new ToolStripMenuItem();
+            mnuLegendTop = new ToolStripMenuItem();
+            mnuLegendBottom = new ToolStripMenuItem();
+            mnuLegendLeft = new ToolStripMenuItem();
+            mnuLegendRight = new ToolStripMenuItem();
+            axesToolStripMenuItem = new ToolStripMenuItem();
+            mnuSeperator = new ToolStripMenuItem();
+            mnuZeroLines = new ToolStripMenuItem();
+            mnuSubSeperator = new ToolStripMenuItem();
+            mnuTicks = new ToolStripMenuItem();
+            mnuSubTicks = new ToolStripMenuItem();
+            mnuSeries = new ToolStripMenuItem();
+            mnuAnimationOnOff = new ToolStripMenuItem();
+            mnuHoverable = new ToolStripMenuItem();
+            mnuSeries1 = new ToolStripMenuItem();
+            mnuSeries2 = new ToolStripMenuItem();
+            dataToolStripMenuItem = new ToolStripMenuItem();
+            mnu360Points = new ToolStripMenuItem();
+            mnu3600Points = new ToolStripMenuItem();
+            mnu36000Points = new ToolStripMenuItem();
+            mnu360000Points = new ToolStripMenuItem();
+            mnuLive = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // chart
+            // 
+            chart.Dock = DockStyle.Fill;
+            chart.Location = new Point(0, 24);
+            chart.Name = "chart";
+            chart.Size = new Size(800, 426);
+            chart.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { mnuInteraction, legendToolStripMenuItem, axesToolStripMenuItem, mnuSeries, dataToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuInteraction
+            // 
+            mnuInteraction.DropDownItems.AddRange(new ToolStripItem[] { mnuZoomAll, mnuZoomX, mnuZoomY, mnuZoomBoth, mnuPan, mnuZoomBothPan, mnuZoomNone });
+            mnuInteraction.Name = "mnuInteraction";
+            mnuInteraction.Size = new Size(76, 20);
+            mnuInteraction.Text = "Interaction";
+            // 
+            // mnuZoomAll
+            // 
+            mnuZoomAll.Name = "mnuZoomAll";
+            mnuZoomAll.Size = new Size(180, 22);
+            mnuZoomAll.Text = "Zoom All";
+            mnuZoomAll.Click += mnuZoomAll_Click;
+            // 
+            // mnuZoomX
+            // 
+            mnuZoomX.Name = "mnuZoomX";
+            mnuZoomX.Size = new Size(180, 22);
+            mnuZoomX.Text = "Zoom X";
+            mnuZoomX.Click += mnuZoomX_Click;
+            // 
+            // mnuZoomY
+            // 
+            mnuZoomY.Name = "mnuZoomY";
+            mnuZoomY.Size = new Size(180, 22);
+            mnuZoomY.Text = "Zoom Y";
+            mnuZoomY.Click += mnuZoomY_Click;
+            // 
+            // mnuZoomBoth
+            // 
+            mnuZoomBoth.Name = "mnuZoomBoth";
+            mnuZoomBoth.Size = new Size(180, 22);
+            mnuZoomBoth.Text = "Zoom Both";
+            mnuZoomBoth.Click += mnuZoomBoth_Click;
+            // 
+            // mnuPan
+            // 
+            mnuPan.Name = "mnuPan";
+            mnuPan.Size = new Size(180, 22);
+            mnuPan.Text = "Pan";
+            mnuPan.Click += mnuPan_Click;
+            // 
+            // mnuZoomBothPan
+            // 
+            mnuZoomBothPan.Name = "mnuZoomBothPan";
+            mnuZoomBothPan.Size = new Size(180, 22);
+            mnuZoomBothPan.Text = "Zoom Both and Pan";
+            mnuZoomBothPan.Click += mnuZoomBothPan_Click;
+            // 
+            // mnuZoomNone
+            // 
+            mnuZoomNone.Name = "mnuZoomNone";
+            mnuZoomNone.Size = new Size(180, 22);
+            mnuZoomNone.Text = "None";
+            mnuZoomNone.Click += mnuZoomNone_Click;
+            // 
+            // legendToolStripMenuItem
+            // 
+            legendToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { mnuLegenHidden, mnuLegendTop, mnuLegendBottom, mnuLegendLeft, mnuLegendRight });
+            legendToolStripMenuItem.Name = "legendToolStripMenuItem";
+            legendToolStripMenuItem.Size = new Size(58, 20);
+            legendToolStripMenuItem.Text = "Legend";
+            // 
+            // mnuLegenHidden
+            // 
+            mnuLegenHidden.Name = "mnuLegenHidden";
+            mnuLegenHidden.Size = new Size(114, 22);
+            mnuLegenHidden.Text = "Hidden";
+            mnuLegenHidden.Click += mnuLegenHidden_Click;
+            // 
+            // mnuLegendTop
+            // 
+            mnuLegendTop.Name = "mnuLegendTop";
+            mnuLegendTop.Size = new Size(114, 22);
+            mnuLegendTop.Text = "Top";
+            mnuLegendTop.Click += mnuLegendTop_Click;
+            // 
+            // mnuLegendBottom
+            // 
+            mnuLegendBottom.Name = "mnuLegendBottom";
+            mnuLegendBottom.Size = new Size(114, 22);
+            mnuLegendBottom.Text = "Bottom";
+            mnuLegendBottom.Click += mnuLegendBottom_Click;
+            // 
+            // mnuLegendLeft
+            // 
+            mnuLegendLeft.Name = "mnuLegendLeft";
+            mnuLegendLeft.Size = new Size(114, 22);
+            mnuLegendLeft.Text = "Left";
+            mnuLegendLeft.Click += mnuLegendLeft_Click;
+            // 
+            // mnuLegendRight
+            // 
+            mnuLegendRight.Name = "mnuLegendRight";
+            mnuLegendRight.Size = new Size(114, 22);
+            mnuLegendRight.Text = "Right";
+            mnuLegendRight.Click += mnuLegendRight_Click;
+            // 
+            // axesToolStripMenuItem
+            // 
+            axesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { mnuSeperator, mnuZeroLines, mnuSubSeperator, mnuTicks, mnuSubTicks });
+            axesToolStripMenuItem.Name = "axesToolStripMenuItem";
+            axesToolStripMenuItem.Size = new Size(44, 20);
+            axesToolStripMenuItem.Text = "Axes";
+            // 
+            // mnuSeperator
+            // 
+            mnuSeperator.Name = "mnuSeperator";
+            mnuSeperator.Size = new Size(144, 22);
+            mnuSeperator.Text = "Seperator";
+            mnuSeperator.Click += mnuSeperator_Click;
+            // 
+            // mnuZeroLines
+            // 
+            mnuZeroLines.Name = "mnuZeroLines";
+            mnuZeroLines.Size = new Size(144, 22);
+            mnuZeroLines.Text = "Zero Lines";
+            mnuZeroLines.Click += mnuZeroLines_Click;
+            // 
+            // mnuSubSeperator
+            // 
+            mnuSubSeperator.Name = "mnuSubSeperator";
+            mnuSubSeperator.Size = new Size(144, 22);
+            mnuSubSeperator.Text = "SubSeperator";
+            mnuSubSeperator.Click += mnuSubSeperator_Click;
+            // 
+            // mnuTicks
+            // 
+            mnuTicks.Name = "mnuTicks";
+            mnuTicks.Size = new Size(144, 22);
+            mnuTicks.Text = "Ticks";
+            mnuTicks.Click += mnuTicks_Click;
+            // 
+            // mnuSubTicks
+            // 
+            mnuSubTicks.Name = "mnuSubTicks";
+            mnuSubTicks.Size = new Size(144, 22);
+            mnuSubTicks.Text = "SubTicks";
+            mnuSubTicks.Click += mnuSubTicks_Click;
+            // 
+            // mnuSeries
+            // 
+            mnuSeries.DropDownItems.AddRange(new ToolStripItem[] { mnuAnimationOnOff, mnuHoverable, mnuSeries1, mnuSeries2 });
+            mnuSeries.Name = "mnuSeries";
+            mnuSeries.Size = new Size(49, 20);
+            mnuSeries.Text = "Series";
+            // 
+            // mnuAnimationOnOff
+            // 
+            mnuAnimationOnOff.Checked = true;
+            mnuAnimationOnOff.CheckState = CheckState.Checked;
+            mnuAnimationOnOff.Name = "mnuAnimationOnOff";
+            mnuAnimationOnOff.Size = new Size(130, 22);
+            mnuAnimationOnOff.Text = "Animation";
+            mnuAnimationOnOff.Click += mnuAnimationOnOff_Click;
+            // 
+            // mnuHoverable
+            // 
+            mnuHoverable.Checked = true;
+            mnuHoverable.CheckState = CheckState.Checked;
+            mnuHoverable.Name = "mnuHoverable";
+            mnuHoverable.Size = new Size(130, 22);
+            mnuHoverable.Text = "Hoverable";
+            mnuHoverable.Click += mnuHoverable_Click;
+            // 
+            // mnuSeries1
+            // 
+            mnuSeries1.Checked = true;
+            mnuSeries1.CheckState = CheckState.Checked;
+            mnuSeries1.Name = "mnuSeries1";
+            mnuSeries1.Size = new Size(130, 22);
+            mnuSeries1.Text = "Series 1";
+            mnuSeries1.Click += mnuSeries1_Click;
+            // 
+            // mnuSeries2
+            // 
+            mnuSeries2.Checked = true;
+            mnuSeries2.CheckState = CheckState.Checked;
+            mnuSeries2.Name = "mnuSeries2";
+            mnuSeries2.Size = new Size(130, 22);
+            mnuSeries2.Text = "Series 2";
+            mnuSeries2.Click += mnuSeries2_Click;
+            // 
+            // dataToolStripMenuItem
+            // 
+            dataToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { mnu360Points, mnu3600Points, mnu36000Points, mnu360000Points, mnuLive });
+            dataToolStripMenuItem.Name = "dataToolStripMenuItem";
+            dataToolStripMenuItem.Size = new Size(43, 20);
+            dataToolStripMenuItem.Text = "Data";
+            // 
+            // mnu360Points
+            // 
+            mnu360Points.Name = "mnu360Points";
+            mnu360Points.Size = new Size(180, 22);
+            mnu360Points.Text = "360 points";
+            mnu360Points.Click += mnu360Points_Click;
+            // 
+            // mnu3600Points
+            // 
+            mnu3600Points.Name = "mnu3600Points";
+            mnu3600Points.Size = new Size(180, 22);
+            mnu3600Points.Text = "3600 points";
+            mnu3600Points.Click += mnu3600Points_Click;
+            // 
+            // mnu36000Points
+            // 
+            mnu36000Points.Name = "mnu36000Points";
+            mnu36000Points.Size = new Size(180, 22);
+            mnu36000Points.Text = "36000 points";
+            mnu36000Points.Click += mnu36000Points_Click;
+            // 
+            // mnu360000Points
+            // 
+            mnu360000Points.Name = "mnu360000Points";
+            mnu360000Points.Size = new Size(180, 22);
+            mnu360000Points.Text = "360000 points";
+            mnu360000Points.Click += mnu360000Points_Click;
+            // 
+            // mnuLive
+            // 
+            mnuLive.Name = "mnuLive";
+            mnuLive.Size = new Size(180, 22);
+            mnuLive.Text = "Live";
+            mnuLive.Click += mnuLive_Click;
+            // 
+            // LiveChartDemo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(chart);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "LiveChartDemo";
+            Text = "LiveChart Demo";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private LiveChartsCore.SkiaSharpView.WinForms.CartesianChart chart;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem mnuInteraction;
+        private ToolStripMenuItem mnuZoomAll;
+        private ToolStripMenuItem mnuZoomX;
+        private ToolStripMenuItem mnuZoomY;
+        private ToolStripMenuItem mnuZoomBoth;
+        private ToolStripMenuItem mnuPan;
+        private ToolStripMenuItem mnuZoomBothPan;
+        private ToolStripMenuItem mnuZoomNone;
+        private ToolStripMenuItem legendToolStripMenuItem;
+        private ToolStripMenuItem mnuLegenHidden;
+        private ToolStripMenuItem mnuLegendTop;
+        private ToolStripMenuItem mnuLegendBottom;
+        private ToolStripMenuItem mnuLegendLeft;
+        private ToolStripMenuItem mnuLegendRight;
+        private ToolStripMenuItem axesToolStripMenuItem;
+        private ToolStripMenuItem mnuSeperator;
+        private ToolStripMenuItem mnuZeroLines;
+        private ToolStripMenuItem mnuSubSeperator;
+        private ToolStripMenuItem mnuTicks;
+        private ToolStripMenuItem mnuSubTicks;
+        private ToolStripMenuItem mnuSeries;
+        private ToolStripMenuItem mnuAnimationOnOff;
+        private ToolStripMenuItem mnuHoverable;
+        private ToolStripMenuItem mnuSeries1;
+        private ToolStripMenuItem mnuSeries2;
+        private ToolStripMenuItem dataToolStripMenuItem;
+        private ToolStripMenuItem mnu360Points;
+        private ToolStripMenuItem mnu3600Points;
+        private ToolStripMenuItem mnu36000Points;
+        private ToolStripMenuItem mnu360000Points;
+        private ToolStripMenuItem mnuLive;
+    }
+}
---
+++ PlottingDemo/LiveChartDemo.cs
@@ -0,0 +1,289 @@
+﻿using LiveChartsCore;
+using LiveChartsCore.Kernel.Sketches;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.Painting.Effects;
+using SkiaSharp;
+using System.Collections.ObjectModel;
+using System.Data;
+
+namespace PlottingDemo
+{
+    public partial class LiveChartDemo : Form
+    {
+        private RectangleF? selectionRectangle = null;
+        private PointF? selectionStart = null;
+
+        private static readonly SKColor s_gray = new(195, 195, 195);
+        private static readonly SKColor s_gray1 = new(160, 160, 160);
+        private static readonly SKColor s_gray2 = new(90, 90, 90);
+        private static readonly SKColor s_dark3 = new(60, 60, 60);
+
+        private SolidColorPaint ZeroPaint = new SolidColorPaint
+        {
+            Color = s_gray1,
+            StrokeThickness = 2
+        };
+
+        private SolidColorPaint SeparatorsPaint = new SolidColorPaint
+        {
+            Color = s_gray,
+            StrokeThickness = 1,
+            PathEffect = new DashEffect(new float[] { 3, 3 })
+        };
+
+        private SolidColorPaint SubseparatorsPaint = new SolidColorPaint
+        {
+            Color = s_gray2,
+            StrokeThickness = 0.5f
+        };
+
+        private SolidColorPaint TicksPaint = new SolidColorPaint
+        {
+            Color = s_gray,
+            StrokeThickness = 1.5f
+        };
+
+        private SolidColorPaint SubticksPaint = new SolidColorPaint
+        {
+            Color = s_gray,
+            StrokeThickness = 1
+        };
+
+        double[] series1Data = { 0 };
+        double[] series2Data = { 0 };
+
+        void ProvideData(int numData)
+        {
+            Task.Run(() =>
+            {
+                series1Data = Enumerable.Range(0, numData).Select(x => Math.Sin((double)x * Math.PI / 180.0)).ToArray();
+                series2Data = Enumerable.Range(0, numData).Select(x => Math.Cos((double)x * Math.PI / 180.0)).ToArray();
+
+                // Create chart
+                chart.Series = new ISeries[]
+                {
+                    new LineSeries<double> { Values = series1Data, Name = "Series 1" },
+                    new LineSeries<double> { Values = series2Data, Name = "Series 2" }
+                };
+            });
+        }
+
+        public LiveChartDemo()
+        {
+            InitializeComponent();
+
+            ProvideData(360);
+            /*
+            // Create 1 million data points for two series
+            double[] series1Data = Enumerable.Range(0, 360).Select(x => Math.Sin((double)x * Math.PI / 180.0)).ToArray();
+            double[] series2Data = Enumerable.Range(0, 360).Select(x => Math.Cos((double)x * Math.PI / 180.0)).ToArray();
+
+            // Create chart
+            chart.Series = new ISeries[]
+            {
+                new LineSeries<double> { Values = series1Data, Name = "Series 1" },
+                new LineSeries<double> { Values = series2Data, Name = "Series 2" }
+            };
+            */
+
+            chart.XAxes = new Axis[]
+            {
+                new Axis
+                {
+                    Name = "X Axis",
+                    NamePaint = new SolidColorPaint(s_dark3),
+                    SubseparatorsCount = 9,
+                }
+            };
+
+            chart.YAxes = new Axis[]
+            {
+                new Axis
+                {
+                    Name = "Y Axis",
+                    SubseparatorsCount = 9,
+                }
+            };
+
+            chart.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.Both;
+            chart.LegendPosition = LiveChartsCore.Measure.LegendPosition.Top;
+        }
+
+        private void mnuZoomAll_Click(object sender, EventArgs e)
+        {
+            ((Axis[])chart.XAxes)[0].MinLimit = null;
+            ((Axis[])chart.XAxes)[0].MaxLimit = null;
+            ((Axis[])chart.YAxes)[0].MinLimit = null;
+            ((Axis[])chart.YAxes)[0].MaxLimit = null;
+        }
+
+        private void mnuZoomX_Click(object sender, EventArgs e)
+        {
+            chart.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.ZoomX;
+        }
+
+        private void mnuZoomY_Click(object sender, EventArgs e)
+        {
+            chart.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.ZoomY;
+        }
+
+        private void mnuZoomBoth_Click(object sender, EventArgs e)
+        {
+            chart.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.ZoomX | LiveChartsCore.Measure.ZoomAndPanMode.ZoomY;
+        }
+
+        private void mnuPan_Click(object sender, EventArgs e)
+        {
+            chart.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.PanX | LiveChartsCore.Measure.ZoomAndPanMode.PanY;
+        }
+
+        private void mnuZoomBothPan_Click(object sender, EventArgs e)
+        {
+            chart.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.Both;
+        }
+
+        private void mnuZoomNone_Click(object sender, EventArgs e)
+        {
+            chart.ZoomMode = LiveChartsCore.Measure.ZoomAndPanMode.None;
+        }
+
+        private void mnuLegenHidden_Click(object sender, EventArgs e)
+        {
+            chart.LegendPosition = LiveChartsCore.Measure.LegendPosition.Hidden;
+        }
+
+        private void mnuLegendTop_Click(object sender, EventArgs e)
+        {
+            chart.LegendPosition = LiveChartsCore.Measure.LegendPosition.Top;
+        }
+
+        private void mnuLegendBottom_Click(object sender, EventArgs e)
+        {
+            chart.LegendPosition = LiveChartsCore.Measure.LegendPosition.Bottom;
+        }
+
+        private void mnuLegendLeft_Click(object sender, EventArgs e)
+        {
+            chart.LegendPosition = LiveChartsCore.Measure.LegendPosition.Left;
+        }
+
+        private void mnuLegendRight_Click(object sender, EventArgs e)
+        {
+            chart.LegendPosition = LiveChartsCore.Measure.LegendPosition.Right;
+        }
+
+        private void mnuSeperator_Click(object sender, EventArgs e)
+        {
+            mnuSeperator.Checked = !mnuSeperator.Checked;
+
+            ((Axis[])chart.XAxes)[0].SeparatorsPaint = mnuSeperator.Checked ? SeparatorsPaint : null;
+            ((Axis[])chart.YAxes)[0].SeparatorsPaint = mnuSeperator.Checked ? SeparatorsPaint : null;
+
+            ((Axis[])chart.XAxes)[0].ShowSeparatorLines = mnuSeperator.Checked;
+            ((Axis[])chart.YAxes)[0].ShowSeparatorLines = mnuSeperator.Checked;
+        }
+
+        private void mnuZeroLines_Click(object sender, EventArgs e)
+        {
+            mnuZeroLines.Checked = !mnuZeroLines.Checked;
+
+            ((Axis[])chart.XAxes)[0].ZeroPaint = mnuZeroLines.Checked ? ZeroPaint : null;
+            ((Axis[])chart.YAxes)[0].ZeroPaint = mnuZeroLines.Checked ? ZeroPaint : null;
+        }
+
+        private void mnuSubSeperator_Click(object sender, EventArgs e)
+        {
+            mnuSubSeperator.Checked = !mnuSubSeperator.Checked;
+
+            ((Axis[])chart.XAxes)[0].SubseparatorsPaint = mnuSubSeperator.Checked ? SubseparatorsPaint : null;
+            ((Axis[])chart.YAxes)[0].SubseparatorsPaint = mnuSubSeperator.Checked ? SubseparatorsPaint : null;
+        }
+
+        private void mnuTicks_Click(object sender, EventArgs e)
+        {
+            mnuTicks.Checked = !mnuTicks.Checked;
+
+            ((Axis[])chart.XAxes)[0].TicksPaint = mnuTicks.Checked ? TicksPaint : null;
+            ((Axis[])chart.YAxes)[0].TicksPaint = mnuTicks.Checked ? TicksPaint : null;
+        }
+
+        private void mnuSubTicks_Click(object sender, EventArgs e)
+        {
+            mnuSubTicks.Checked = !mnuSubTicks.Checked;
+
+            ((Axis[])chart.XAxes)[0].SubticksPaint = mnuSubTicks.Checked ? SubticksPaint : null;
+            ((Axis[])chart.YAxes)[0].SubticksPaint = mnuSubTicks.Checked ? SubticksPaint : null;
+        }
+
+        private void mnuAnimationOnOff_Click(object sender, EventArgs e)
+        {
+            mnuAnimationOnOff.Checked = !mnuAnimationOnOff.Checked;
+            foreach (var ser in chart.Series)
+                ser.AnimationsSpeed = mnuAnimationOnOff.Checked ? TimeSpan.FromMilliseconds(200) : TimeSpan.FromMilliseconds(0);
+        }
+
+        private void mnuHoverable_Click(object sender, EventArgs e)
+        {
+            mnuHoverable.Checked = !mnuHoverable.Checked;
+            foreach (var ser in chart.Series)
+                ser.IsHoverable = mnuHoverable.Checked;
+        }
+
+        private void mnuSeries1_Click(object sender, EventArgs e)
+        {
+            mnuSeries1.Checked = !mnuSeries1.Checked;
+            ((ISeries[])chart.Series)[0].IsVisible = mnuSeries1.Checked;
+        }
+
+        private void mnuSeries2_Click(object sender, EventArgs e)
+        {
+            mnuSeries2.Checked = !mnuSeries2.Checked;
+            ((ISeries[])chart.Series)[1].IsVisible = mnuSeries2.Checked;
+        }
+
+        private void mnu360Points_Click(object sender, EventArgs e)
+        {
+            ProvideData(360);
+        }
+
+        private void mnu3600Points_Click(object sender, EventArgs e)
+        {
+            ProvideData(3600);
+        }
+
+        private void mnu36000Points_Click(object sender, EventArgs e)
+        {
+            ProvideData(36000);
+        }
+
+        private void mnu360000Points_Click(object sender, EventArgs e)
+        {
+            ProvideData(360000);
+        }
+
+        private void mnuLive_Click(object sender, EventArgs e)
+        {
+            var liveData = new ObservableCollection<double> { 1, 2, 3, 4, 5 };
+
+            chart.Series = new ISeries[]
+            {
+                    new LineSeries<double> { Values = liveData, Name = "Series 1" },
+            };
+
+            // Add new data points dynamically
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        liveData.Add(new Random().NextDouble() * 10);
+                        if (liveData.Count > 50) liveData.RemoveAt(0); 
+                    }));
+                    Thread.Sleep(500);
+                }
+            });
+        }
+    }
+}
